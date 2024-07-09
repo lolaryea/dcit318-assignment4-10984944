dd1156a6ed4f908dc98b6052df07ccac3d26af9b
Leslie Otoo Laryea <102968653+lolaryea@users.noreply.github.com>
2024-07-09 20:57:14 +0000

    dope

---
+++ Canvas Drawing Application/Canvas Drawing Application/Form1.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace DrawingApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.drawingPanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+
+            // 
+            // drawingPanel
+            // 
+            this.drawingPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.drawingPanel.Location = new System.Drawing.Point(0, 0);
+            this.drawingPanel.Name = "drawingPanel";
+            this.drawingPanel.Size = new System.Drawing.Size(800, 450);
+            this.drawingPanel.TabIndex = 0;
+            this.drawingPanel.BackColor = System.Drawing.Color.White;
+            this.drawingPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.drawingPanel_MouseDown);
+            this.drawingPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.drawingPanel_MouseMove);
+            this.drawingPanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.drawingPanel_MouseUp);
+
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.drawingPanel);
+            this.Name = "Form1";
+            this.Text = "Drawing App";
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Panel drawingPanel;
+    }
+}
---
+++ Canvas Drawing Application/Canvas Drawing Application/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DrawingApp
+{
+    public partial class Form1 : Form
+    {
+        private bool isDrawing;
+        private Point lastPoint;
+        private Graphics graphics;
+
+        public Form1()
+        {
+            InitializeComponent();
+            graphics = drawingPanel.CreateGraphics();
+        }
+
+        private void drawingPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isDrawing = true;
+                lastPoint = e.Location;
+            }
+        }
+
+        private void drawingPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDrawing)
+            {
+                Pen pen = new Pen(Color.Black, 2);
+                graphics.DrawLine(pen, lastPoint, e.Location);
+                lastPoint = e.Location;
+            }
+        }
+
+        private void drawingPanel_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isDrawing = false;
+            }
+        }
+    }
+}
